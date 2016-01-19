320a28c3aadf6fe0eb41307f04959590ebede2be
Christian Olsson <chrols@chrols.se>
2016-01-19 14:46:30 +0100

    emacs: update init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,10 +2,7 @@
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit
-                      starter-kit-lisp
-                      starter-kit-bindings
-                      starter-kit-eshell
+(defvar my-packages '(
                       jedi
                       color-theme
                       color-theme-monokai
@@ -23,8 +20,8 @@
 (load "~/.emacs.d/utility.el")
 
 (require 'color-theme)
-(color-theme-molokai)
-; (color-theme-monokai)
+;(color-theme-molokai)
+(color-theme-monokai)
 
 (global-set-key [f2] 'visit-ansi-term)
 (global-set-key [f11] 'fullscreen)
@@ -43,6 +40,7 @@
 
 (setq python-shell-interpreter "/usr/bin/python2")
 (setq-default show-trailing-whitespace t)
+
 (global-auto-complete-mode t)
 
 (setq load-path (cons "~/.emacs.d/git/powerline" load-path))
@@ -67,14 +65,27 @@
 
 (put 'upcase-region 'disabled nil)
 (global-set-key [f4] 'ff-find-other-file)
-(setq show-trailing-whitespace t)
 
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/git/ethan-wspace/lisp"))
-(require 'ethan-wspace)
-(global-ethan-wspace-mode 1)
+;(add-to-list 'load-path (expand-file-name "~/.emacs.d/git/ethan-wspace/lisp"))
+;(require 'ethan-wspace)
+;(global-ethan-wspace-mode 1)
 
 (require 'whitespace)
 (setq whitespace-line-column 80) ;; limit line length
 (setq whitespace-style '(face lines-tail))
-
+(setq whitespace-style '(face lines-tail tab-mark lines trailing)) 
 (add-hook 'prog-mode-hook 'whitespace-mode)
+
+(setq load-path (cons  "/usr/lib/erlang/lib/tools-2.8.1/emacs"
+                       load-path))
+(setq erlang-root-dir "/usr/lib/erlang")
+(setq exec-path (cons "/usr/lib/erlang/bin" exec-path))
+(require 'erlang-start)
+;disable backup
+(setq backup-inhibited t)
+;disable auto save
+(setq auto-save-default nil)
+
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
+
+(setq x-select-enable-clipboard t)
