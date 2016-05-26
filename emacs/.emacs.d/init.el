612f918bf98851812bf4dd88c70d8e9bc2b4fdef
Christian Olsson <chrols@chrols.se>
2016-05-26 23:24:55 +0200

    emacs: add go-mode & go related config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -94,3 +94,33 @@
 
 (setq-default tab-width 4)
 
+(setenv "GOPATH" "/home/chrols/golang")
+
+(setq exec-path (cons "/usr/lib/go/bin" exec-path))
+(add-to-list 'exec-path "/home/chrols/golang/bin")
+
+(defun go-run-buffer()
+  (interactive)
+  (shell-command (concat "go run " (buffer-name))))
+
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (local-set-key (kbd "C-c C-c")  'go-run-buffer)
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(load "~/.emacs.d/go-autocomplete.el")
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+(with-eval-after-load 'go-mode
+  (require 'go-autocomplete))
