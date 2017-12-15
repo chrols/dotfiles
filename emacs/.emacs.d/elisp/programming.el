108601d3536b10c5a9d29e6e8cdcd257c1ea3d89
Christian Olsson <chrols@chrols.se>
2017-12-15 08:54:06 +0100

    emacs: set flycheck language standard to C++11

--- emacs/.emacs.d/elisp/programming.el
+++ emacs/.emacs.d/elisp/programming.el
@@ -6,6 +6,13 @@
 (setq whitespace-style '(face lines-tail tab-mark lines trailing))
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
+;; Require flycheck to be present
+(require 'flycheck)
+;; Force flycheck to always use c++11 support. We use
+;; the clang language backend so this is set to clang
+(add-hook 'c++-mode-hook
+          (lambda () (setq flycheck-clang-language-standard "c++11")))
+;; Turn flycheck on everywhere
 (global-flycheck-mode)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
